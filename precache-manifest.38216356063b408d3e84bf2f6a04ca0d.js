de10bded2af64f6a48f992b0e46ea66e6f058684
suhmantha1 <suhmantha1@gmail.com>
2020-11-20 10:41:08 -0500

    Updates

---
+++ precache-manifest.38216356063b408d3e84bf2f6a04ca0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cda2fae471e49e5e5b37200050dc0d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f48fb66cb2005a6d158",
+    "url": "./static/css/main.5e07a5f7.chunk.css"
+  },
+  {
+    "revision": "02be9479dc801bb86486",
+    "url": "./static/js/2.400e5c77.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "./static/js/2.400e5c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f48fb66cb2005a6d158",
+    "url": "./static/js/main.df0364a6.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.df0364a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2b6fa42268d1595971",
+    "url": "./static/js/runtime-main.bfaed358.js"
+  }
+]);
